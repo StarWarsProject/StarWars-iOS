395f5a2db31411856fc2ab748b389325a3cc355e
YawarValeriano <yawarvs@gmail.com>
2022-11-22 08:22:06 -0600

    Fixed lint errors

--- StarWars-iOS/SceneDelegate.swift
+++ StarWars-iOS/SceneDelegate.swift
@@ -12,7 +12,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
 
         self.window?.rootViewController = getRootViewController()
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
@@ -42,6 +41,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         return navController
     }
 
-
 }
-
